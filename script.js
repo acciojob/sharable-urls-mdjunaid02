6bb31c3b20fa5b9368e252189431b68e2569d9d2
mdjunaid02 <156643108+mdjunaid02@users.noreply.github.com>
2026-01-11 19:35:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // your code here
+document.getElementById("button").onclick=function(){
+	var name=document.getElementById("name").value;
+	var year=document.getElementById("year").value;
+	document.getElementById("url").innerText="https://localhost:8080/?name="+name+"&year"+year;
+}
